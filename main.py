a9d24fb464a0a7fd05f8e5b681aebd21a2eeba14
Worsbee <jworsley.personal@outlook.com>
2023-11-01 14:07:32 +0000

    sample project for testing version 2

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 """
 This project has been changed for testing
 """
+
+vertices = ['s', 't', 'x', 'y', 'z']
+edges = [('s', 't', 10), ('s', 'y', 5), ('t', 'x', 1), ('t', 'y', 2), ('x', 'z', 4),
+         ('y', 't', 3), ('y', 'x', 9), ('y', 'z', 2), ('z', 's', 7), ('z', 'x', 6)]
+
+dijkstra(edges, vertices)
+
