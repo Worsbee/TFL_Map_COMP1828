4f95b6511aea90dd4bdbbf4b99d015d7001f60f0
fammok21 <149542653+fammok21@users.noreply.github.com>
2023-11-14 10:32:41 +0000

    Update Task_1a.py

--- Task_1a.py
+++ Task_1a.py
@@ -1,5 +1,7 @@
-from data_load import bidirectional_edges
-from data_load import stations_1
+# import the excel data in the form of a list for the stations (vertices) and a nested list for each station's adjacent station plus the time to get from one station
+# to another (the edges of the graph).
+from data_load import bidirectional_edges  # edges
+from data_load import stations_1           # vertices
 from single_source_shortest_paths import initialize_single_source, relax
 from min_heap_priority_queue import MinHeapPriorityQueue
 import time
@@ -8,6 +10,18 @@ start_time = time.time()
 # Assuming the code for print_path is available
 
 def dijkstra(G, s, end):
+    """Solve single-source shortest-paths problem with no negative-weight edges.
+
+	Arguments:
+	G -- a directed, weighted graph
+	s -- index of source vertex
+	Assumption:
+	All weights are nonnegative
+
+	Returns:
+	d -- distances from source vertex s
+	pi -- predecessors
+	"""
     card_V = G.get_card_V()
 
     d, pi = initialize_single_source(G, s)
@@ -25,10 +39,12 @@ def dijkstra(G, s, end):
             relax(u, v, edge.get_weight(), d, pi,
                   lambda v: queue.decrease_key(v, d[u] + edge.get_weight()))
 
-    return d, pi
+    return d, pi  # returns the shortest distance from one station to another, plus a list storing all the predecessors of the ending station
 
 
-def print_path(pi, s, v, mapping_func):
+def print_path(pi, s, v, mapping_func):  
+    # this function takes in the predecessor list, the indexes of the starting and destination stations
+    #to print the path from one station to another (this function is used to give a list of all the stations the user passes to get to their destination)
     if v == s:
         return [mapping_func(s)]
     elif pi[v] is None:
@@ -40,31 +56,34 @@ def print_path(pi, s, v, mapping_func):
 if __name__ == "__main__":
     from adjacency_list_graph import AdjacencyListGraph
 
-    vertices = stations_1
-    edges = bidirectional_edges
+    vertices = stations_1       # list of all the stations
+    edges = bidirectional_edges # nested list in the structure (station1, station2, time)
 
-    starting_v = input("Enter the station you are starting from: ")
+    starting_v = input("Enter the station you are starting from: ")  # takes user travel input
     if starting_v not in vertices:
-        print("This node does not exist")
+        print("This node does not exist")  # checks if the station entered by the user exists in the vertices list
     ending_v = input("Enter a destination station: ")
     if ending_v not in vertices:
         print("This node does not exist")
 
+     # create a graph storing all the connections between the stations so dijkstra's algorithm can calculate the shortes paths
     graph1 = AdjacencyListGraph(len(vertices), True, True)
     for edge in edges:
-        graph1.insert_edge(vertices.index(edge[0]), vertices.index(edge[1]), edge[2])
+        graph1.insert_edge(vertices.index(edge[0]), vertices.index(edge[1]), edge[2])  
 
-    d, pi = dijkstra(graph1, vertices.index(starting_v), vertices.index(ending_v))
+    d, pi = dijkstra(graph1, vertices.index(starting_v), vertices.index(ending_v))  # run dijkstra's algorithm
 
     if d[vertices.index(ending_v)] != float('inf'):
+        # prints shortest distance in minutes from one station to another
         print(f"Shortest distance from {starting_v} to {ending_v} is {d[vertices.index(ending_v)]} minutes")
         # Get the path from the starting to ending vertices
+        # run path function
         path = print_path(pi, vertices.index(starting_v), vertices.index(ending_v), lambda i: vertices[i])
 
         # Print the result
         if path is not None:
             result = ", ".join(map(str, path))
-            print(f"Shortest path from {starting_v} to {ending_v}: {result}")
+            print(f"Shortest path from {starting_v} to {ending_v}: {result}")  # prints the path from one station to another
         else:
             print(f"No path from {starting_v} to {ending_v}")
     else:
@@ -72,4 +91,4 @@ if __name__ == "__main__":
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"{elapsed_time} seconds")
+    print(f"{elapsed_time} seconds")  # prints running time of the algorithm
