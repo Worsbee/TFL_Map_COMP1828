157a9a1c2029e6062603ed1227320eb5bb354026
Worsbee <jworsley.personal@outlook.com>
2023-11-22 03:32:36 +0000

    Moved repeated lines to functions. Progress on task 4

--- Task_4a.py
+++ Task_4a.py
@@ -2,12 +2,30 @@
 Description: Finding stations to remove without heavily impacting feasibility
 """
 
-from data_load import task_4_edges, stations, stations_clean, bidirectional_edges
+"""
+Notes
+
+AdjacentListGraph returns a graph in the format of 'Station[n]: connecting stations (minutes)'
+for example Harrow & Wealdstone(stations[0]): 'Kenton'(stations[1]) (2 minutes) == '0: 1 (2)'
+
+Bfs returns the distance of each stations from the source.
+"""
+
+
+from data_load import *
 from bfs import bfs
 from adjacency_list_graph import AdjacencyListGraph
 
 if __name__ == "__main__":
-    for edge in task_4_edges:
-        pass
-    print(bidirectional_edges)
-    #AdjacencyListGraph(task_4_edges, stations)
+    # Rename variables to improve readability
+    vertices = stations
+    edges = bidirectional_edges
+
+    # Produces a graph
+    graph1 = get_graph(vertices, edges)
+
+    print(stations)
+    result = bfs(graph1, 13)
+
+    print(result)
+
