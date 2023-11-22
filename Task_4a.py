2020a362a8fe854ecae6ab7bee4745d449ae03d9
Worsbee <jworsley.personal@outlook.com>
2023-11-22 21:15:15 +0000

    Task 4 Updated

--- Task_4a.py
+++ Task_4a.py
@@ -13,21 +13,62 @@ the first element of the list is stations[0]. The numbers in the place of statio
 input source in stops.
 """
 
-
 from data_load import *
 from bfs import bfs
-from adjacency_list_graph import AdjacencyListGraph
+
+
+def remove_station(source_station, destination_station):
+    pass
+
 
 if __name__ == "__main__":
     # Rename variables to improve readability
     vertices = stations
     edges = bidirectional_edges
 
+    # Collects input for user
+    # rm_source_station = input("What is the source station: ")
+    # rm_destination_station = input("What is the destination station: ")
+
+    # Get all entries of both stations in edges
+    # source_destination = [element for element in source if rm_destination_station in element]
+
+    # Gets numerical index of each station pair in edges
+    # source_index0 = edges.index(source_destination[0])
+    # source_index1 = edges.index(source_destination[1])
+
     # Produces a graph
     graph1 = get_graph(vertices, edges)
 
-    print(stations)
-    result = bfs(graph1, 13)
+    # Produces a breath first search from source and destination
+    result = bfs(graph1, 0)
 
     print(result)
 
+    i = 0
+    for x in result:
+        i += 1
+
+    print(i)
+
+    """
+    result1 = bfs(graph1, destination_index)
+
+    remove_station(source, destination)
+
+    # Produces a graph
+    graph2 = (get_graph(vertices, edges))
+
+    # Produces a breath first search from source and destination
+    rm_result = bfs(graph1, source_index)
+    rm_result1 = bfs(graph1, destination_index)
+
+ 
+    print(edges)
+
+    print(result)
+    print(rm_result)
+
+    print(result1)
+    print(rm_result1)
+"""
