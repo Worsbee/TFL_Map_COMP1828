4dd3bc713f986b9b0da4d05baf3f455600888fcb
Worsbee <jworsley.personal@outlook.com>
2023-11-23 00:07:47 +0000

    Updated Task4

--- Task_4a.py
+++ Task_4a.py
@@ -18,8 +18,7 @@ from bfs import bfs
 
 
 def remove_station(edge_list, source_station, destination_station):
-    print(source_station, destination_station)
-
+    """Removes the station connection from the edges list"""
     # Filter edges to including only source_station and destination_station
     source_list = [item for item in edge_list if source_station in item]
     destination_list = [item for item in edge_list if destination_station in item]
@@ -33,15 +32,35 @@ def remove_station(edge_list, source_station, destination_station):
                 source_destination_list.append(element)
                 break
 
-    # Gets numerical index of each station pair in edges
-    source_index0 = edge_list.index(source_destination_list[0])
-    source_index1 = edge_list.index(source_destination_list[1])
+    try:
+        # Gets numerical index of each station pair in edges
+        source_index0 = edge_list.index(source_destination_list[0])
+        source_index1 = edge_list.index(source_destination_list[1])
+
+    except IndexError:
+        print("The stations list are not adjacent on any line")
+        exit()
 
     for element in source_destination_list:
         edge_list.remove(element)
 
     return edge_list, source_index0, source_index1
 
+def viability_test(rbs, rbd, ras, rad):
+    """
+    Tests the viability of a connection closure.
+    Variables are shorthand for "result"_"before/after"_"source/destination"
+    """
+
+    # Counts the number of impossible stations before and after the change
+    result_before = str(rbs[0]).count('inf') + str(rbs[1]).count('inf')
+    result_after = str(ras[0]).count('inf') + str(ras[1]).count('inf')
+
+    # Rejects any changes which increase the number of impossible stations
+    if result_before is not result_after:
+        message = "Shut down is not possible as some stations become inaccessible"
+
+    return message
 
 if __name__ == "__main__":
     # Rename variables to improve readability
@@ -55,9 +74,14 @@ if __name__ == "__main__":
     # Produces a graph
     graph1 = get_graph(vertices, edges)
 
-    # Produces a breath first search from source and destination
-    result_before_source = bfs(graph1, vertices.index(rm_source_station))
-    result_before_destination = bfs(graph1, vertices.index(rm_destination_station))
+    try:
+        # Produces a breath first search from source and destination
+        result_before_source = bfs(graph1, vertices.index(rm_source_station))
+        result_before_destination = bfs(graph1, vertices.index(rm_destination_station))
+
+    except ValueError:
+        print("One or both stations are not recognised")
+        exit()
 
     updated_edges, index0, index1 = remove_station(edges, rm_source_station, rm_destination_station)
 
@@ -68,8 +92,14 @@ if __name__ == "__main__":
     result_after_source = bfs(graph2, vertices.index(rm_source_station))
     result_after_destination = bfs(graph2, vertices.index(rm_destination_station))
 
+    feasible = viability_test(result_before_source, result_before_destination, result_after_source, result_after_destination)
+
     # Print the results
-    print(f"Source station before change: \n {result_before_source}")
-    print(f"Destination station before change: \n {result_before_source}")
-    print(f"Source station after change: \n {result_after_source}")
-    print(f"Destination station after change: \n {result_after_destination}")
+    print(f"Results for closure between: {rm_source_station} -- {rm_destination_station}")
+    print(feasible)
+
+
+    print(f"Source station before change: {result_before_source}")
+    print(f"Destination station before change: {result_before_source}")
+    print(f"Source station after change: {result_after_source}")
+    print(f"Destination station after change: {result_after_destination}")
