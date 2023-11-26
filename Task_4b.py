2bd398ef66c00fca28d0c02f9a01651304242ff8
Worsbee <jworsley.personal@outlook.com>
2023-11-26 02:08:10 +0000

    Task 4b finished

--- Task_4b.py
+++ Task_4b.py
@@ -5,9 +5,5 @@ from Task_4a import task_4a
 from Task_3a import task_3a
 from data_load import *
 
+# Call task 4a and setting the task 4b conditions to true
 task_4a(stations, bidirectional_edges, True)
-
-# print(task4a)
-# print(f"Journey Time: {task3_time} minutes")
-# print(f"Impact on journey time: +{impact} minutes")
-
